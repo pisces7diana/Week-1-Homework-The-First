c2e27674484b8424886769787d86ea08c4de106c
pisces7diana <email7diana@gmail.com>
2023-11-07 21:05:25 -0800

    Week 1 Homework, The First

---
+++ terminal_homework.bash/first_js_homework.js
@@ -0,0 +1,109 @@
+// Section 1
+    // 1. DRY stands for Don't Repeat Yourself. We should pay attention to it because efficiency matters when it comes to writing thousands of codes. The programming tools we have learned to write DRY code is while and for loops
+    // The difference between const and let and var are as follows: const variables don't change (no reassignment of the variable); let variables can change later (reassign the variable), var is outdated
+
+
+
+// Section 2
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kelvin';
+const f = false;
+const g = 0
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(4 < 53 < 57);
+console.log(a * a == d);
+console.log(e != 'Kevin');
+console.log(48 == '48');
+console.log(!false || 'Kelvin')
+console.log(g)
+
+// I used const because
+// If I didn't use const, let or var, it consoled back the following error:
+    // ReferenceError: a is not defined
+    // at Object.<anonymous> (/Users/dianahudson/Desktop/first_homework/first_js_homework.js/terminal_homework.bash/first_js_homework.js:7:13)
+    // at Module._compile (node:internal/modules/cjs/loader:1241:14)
+    // at Module._extensions..js (node:internal/modules/cjs/loader:1295:10)
+    // at Module.load (node:internal/modules/cjs/loader:1091:32)
+    // at Module._load (node:internal/modules/cjs/loader:938:12)
+    // at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+    // at node:internal/main/run_main_module:23:47
+
+
+
+// Section 3
+
+// Infinite loop?
+// while (true) {
+	// console.log('Do not run this loop');
+// }
+    // The code is an infinite loop because of the while condition is true.
+
+// Infinite lopp II
+    const runProgram = true;
+
+    while (runProgram) {
+        console.log('Do not run this loop');
+        runProgram = false;
+    }
+    // This is not an infinite loop because the condition "false" puts a break to it.
+    // The expected output is the following:
+    // Do not run this loop
+    // Users/dianahudson/Desktop/first_homework/first_js_homework.js/terminal_homework.bash/first_js_homework.js:47
+    // runProgram = false;
+    
+    // TypeError: Assignment to constant variable.
+    // at Object.<anonymous> (/Users/dianahudson/Desktop/first_homework/first_js_homework.js/terminal_homework.bash/first_js_homework.js:47:16)
+    // at Module._compile (node:internal/modules/cjs/loader:1241:14)
+    // at Module._extensions..js (node:internal/modules/cjs/loader:1295:10)
+    // at Module.load (node:internal/modules/cjs/loader:1091:32)
+    // at Module._load (node:internal/modules/cjs/loader:938:12)
+    // at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+    // at node:internal/main/run_main_module:23:47
+    
+    // Node.js v20.9.0
+
+// Reading code
+// Code:
+let letters = "A"; // declare a variable such as letters and set it to equal to a string of A
+let i = 0; // begin the intial condition at 0
+
+while (i < 20) { // start a while loop that will run as long as i is less than 20
+	letters += "A"; // prints the value of letters to the Terminal
+	i++; // after each loop, the initial condition is increased by 1
+} // close the while loop, the code block inside this loop will keep running until the while condition is false
+
+console.log(letters); // console log the value of letters for each time the loop runs
+// Expected result: AAAAAAAAAAAAAAAAAAAA
+// Run the code: Terminal consoled logged as expected
+// Yes, the code was what I expected.
+
+
+// Section 4
+// For loops
+    // A for loop performs the same operation as a while loop. But the key differences are The similarities?
+ // For loop control panel   
+    // For loop control panel, the three components are:
+    // The first part of the control panel is initial condition;
+    // The second part of the control is while condition;
+    // The third part of the control panel is repeating expression;
+// For loop II 
+    for (let num = 0; num <= 999; num++) {
+        console.log(num);
+    }
+// For loop in reverse
+    for (let i = 999; i >= 0; i--) {
+        console.log(i)
+    }
+// More counting
+for (let i = 0; i < 10; i++) {
+    console.log('The value of i is: i of 10');
+}
+// Iteration
+const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
---
+++ terminal_homework.bash/terminal_homework.bash
@@ -0,0 +1,71 @@
+# Copy this text into your `.bash` homework file. Copy and paste the code you successfully used into terminal below each prompt
+
+# Open the **Terminal app**
+// command + space
+// type terminal in Spotlight Search and press Return
+// pwd
+// cd Desktop
+
+# Create a new directory on your Desktop called `first_homework`and enter it
+// mkdir first_homework
+// type ls to make sure first_homework is in Desktop folder
+// cd first_homework
+
+# In `first_homework`, make a directory named `first_js_homework.js`
+
+// mkdir first_js_homework.js
+// type ls to make sure first_js_homework.js is in first_homework folder
+
+# Inside of `first_js_homework.js` make a directory called `terminal_homework.bash`
+// cd first_js_homework.js 
+// mkdir terminal_homework.bash
+// type ls to make sure terminal_homework.bash is in first_js_homework.js folder
+// cd terminal_homework.bash
+
+# Rename `ben_kenobi.txt` to `obi_wan.txt
+
+# Copy `storm_trooper.txt` from `death_star` to `tatooine`
+
+# Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+
+# Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
+
+# Move `millenium_falcon` into `death_star`
+
+# Move `princess_leia.txt` into the `millenium_falcon`
+
+# Delete `obi_wan.txt`
+
+# In `galaxy_far_far_away`, make a directory called `yavin_4`
+
+# Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+
+# Make a directory in `yavin_4` called `x_wing`
+
+# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+
+# Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+
+# In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+
+# Move `darth_vader.txt` into `tie_fighter_1`
+
+# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+
+# Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+
+# Be careful with this command - cannot undo!
+
+# Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
+
+# This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
+
+# Remove `tie_fighters` 2 and 3.
+
+# Touch a file in "**x_wing**" called "**the_force.txt**".
+
+# Destroy the "**death_star**" and anyone inside of it.
+
+# Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+
+# Celebrate!

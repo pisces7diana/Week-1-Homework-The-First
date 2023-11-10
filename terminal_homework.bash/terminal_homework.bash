394d4461809bbd0cb3b94e6d2c8802f46d16a86d
pisces7diana <149442684+pisces7diana@users.noreply.github.com>
2023-11-09 17:28:19 -0800

    Add files via upload

--- terminal_homework.bash/terminal_homework.bash
+++ terminal_homework.bash/terminal_homework.bash
@@ -22,3 +22,110 @@
 // mkdir terminal_homework.bash
 // type ls to make sure terminal_homework.bash is in first_js_homework.js folder
 // cd terminal_homework.bash
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Copy this text into your `.bash` homework file. Copy and paste the code you successfully used into terminal below each prompt
+
+# Open the **Terminal app**
+command + space bar and type Terminal in Spotlight Search
+ls to see my Folders
+cd Desktop
+
+# Create a new directory on your desktop called `galaxy_far_far_away`and enter it
+mkdir galaxy_far_far_away
+ls to make sure galaxy_far_far_away is on my Desktop
+cd galaxy_far_far_away
+
+# Create a directory called `death_star`
+mkdir death_star
+ls to make sure death_star is inside of galaxy_far_far_away
+cd death_star
+
+
+# and make the following files inside of it:
+# darth_vader.txt
+# princess_leia.txt
+# storm_trooper.txt
+touch darth_vader.txt princess_leia.txt storm_trooper.txt
+ls to make sure those files are inside of death_star
+cd .. to go back to galaxy_far_far_away
+
+# In `galaxy_far_far_away`, make a directory named `tatooine`
+mkdir tatooine
+ls to make sure tatooine is inside of galaxy_far_far_away
+cd tatooine
+
+# and create the following files in it:
+# luke.txt
+# ben_kenobi.txt
+touch luke.txt ben_kenobi.txt
+ls to make sure those files are inside of tatooine
+
+# Inside of `tatooine` make a directory called `millenium_falcon`
+# and in it create:
+# han_solo.txt
+# chewbaca.txt
+mkdir millenium_falcon
+ls to make sure millenium_falcon is inside of tatooine
+cd millenium_falcon
+touch han_solo.txt chewbaca.txt
+ls to make sure those files are inside of millenium_falcon
+
+# Rename `ben_kenobi.txt` to `obi_wan.txt
+cd Desktop/galaxy_far_far_away/tatooine
+ls
+mv ben_kenobi.txt obi_wan.txt
+ls to see that the files in there are luke.txt millenium_falcon	obi_wan.txt
+cd ~
+cd Desktop/galaxy_far_far_away/death_star
+ls
+
+# Copy `storm_trooper.txt` from `death_star` to `tatooine`
+cp storm_trooper.txt ../tatooine
+cd ../tatooine
+ls
+
+# Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+
+# Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
+
+# Move `millenium_falcon` into `death_star`
+
+# Move `princess_leia.txt` into the `millenium_falcon`
+
+# Delete `obi_wan.txt`
+
+# In `galaxy_far_far_away`, make a directory called `yavin_4`
+
+# Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+
+# Make a directory in `yavin_4` called `x_wing`
+
+# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+
+# Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+
+# In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+
+# Move `darth_vader.txt` into `tie_fighter_1`
+
+# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+
+# Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+
+# Be careful with this command - cannot undo!
+
+# Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
+
+# This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
+
+# Remove `tie_fighters` 2 and 3.
+
+# Touch a file in "**x_wing**" called "**the_force.txt**".
+
+# Destroy the "**death_star**" and anyone inside of it.
+
+# Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+
+# Celebrate!
